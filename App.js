121295b921f75ae59d46713322ae49ccf2f2bc42
Batuhan ÖZALP <batuhan.ozalp.9@yandex.com>
2022-10-07 22:31:22 +0300

    init

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ Batuhan OZALP - github.com/bozalp
+ */
+
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+import Instagram from './InstagramUI/Instagram';
+const App = () => {
+  return (
+      <Instagram />
+  );
+};
+
+export default App;
